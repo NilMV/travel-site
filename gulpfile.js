ce7b1d448016cee175ce3c900fb8901d8fcea904
NilMV <nil.medvedev17@gmail.com>
2017-10-18 23:52:00 +0300

    Work with CSS architecutre + gulp import

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ postcss = require('gulp-postcss');
 autoprefixer = require('autoprefixer');
 cssvars = require('postcss-simple-vars');
 nested = require('postcss-nested');
+cssImport = require('postcss-import');
 
 gulp.task('html', function(){
 
@@ -11,7 +12,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([autoprefixer, cssvars, nested]))
+  .pipe(postcss([cssImport, autoprefixer, cssvars, nested]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
